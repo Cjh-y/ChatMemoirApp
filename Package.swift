a8ba438d87108e67d0926e7cbb85e9a533adbfcc
cjh <macos@macosdeiMac.local>
2026-07-04 11:03:18 +0800

    Fix: self-contained models, dark mode everywhere, stable page curl

--- Package.swift
+++ Package.swift
@@ -2,12 +2,8 @@
 import PackageDescription
 let package = Package(
     name: "ChatMemoirApp",
-    platforms: [.iOS(.v17), .macOS(.v13)],
+    platforms: [.iOS(.v17)],
     products: [.executable(name: "ChatMemoirApp", targets: ["ChatMemoirApp"])],
-    dependencies: [.package(path: "../ChatMemoir")],
-    targets: [.executableTarget(name: "ChatMemoirApp",
-        dependencies: [.product(name: "TimelineEngine", package: "ChatMemoir"),
-                       .product(name: "StoryEngine", package: "ChatMemoir"),
-                       .product(name: "PresentationEngine", package: "ChatMemoir")],
-        path: "Sources")]
+    dependencies: [],
+    targets: [.executableTarget(name: "ChatMemoirApp", dependencies: [], path: "Sources")]
 )
